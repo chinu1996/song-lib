071452931fec6f4a3a209fa0cf3f7cc3f116f4f4
das <pkd1964@gmail.com>
2017-09-12 03:58:31 +0530

    login functionality

---
+++ client/src/components/Register.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-layout coloumn>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2">
+          <input type="text" name="email" placeholder="email" v-model="email"><br>
+          <input type="password" name="password" placeholder="password" v-model="password"><br>
+          <div class="error" v-html="error"></div>
+          <v-btn class="cyan" @click="register">Register</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  name: 'hello',
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: null
+    }
+  },
+  methods: {
+    async register () {
+      try {
+        const response = await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+        console.log(response.data)
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.error {
+  color: red;
+}
+</style>
---
+++ server/src/config/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_NAME || 'songtracker',
+    user: process.env.DB_USER ||'songtracker',
+    password: process.env.DB_PASS || 'songtracker',
+    options: {
+      dialect: process.env.DIALECT || 'sqlite',
+      host: process.env.HOST || 'localhost',
+      storage: './songtracker.sqlite'
+
+    }
+  }
+}
